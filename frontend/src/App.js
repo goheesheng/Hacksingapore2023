949f14ce8f1832ca121d709be07e877b66c9f78e
Goh Ee Sheng <goheesheng@hotmail.com>
2023-05-12 14:14:37 +0800

    linting

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -75,17 +75,16 @@ const App = () => {
               </button>
             </TooltipComponent>
           </div>
-          { isConnected && (
-          activeMenu ? (
-            <div className="w-72 fixed sidebar dark:bg-secondary-dark-bg bg-white ">
-              <Sidebar />
-            </div>
-          ) : (
-            <div className="w-0 dark:bg-secondary-dark-bg">
-              <Sidebar />
-            </div>
-          )
-          )}
+          {isConnected &&
+            (activeMenu ? (
+              <div className="w-72 fixed sidebar dark:bg-secondary-dark-bg bg-white ">
+                <Sidebar />
+              </div>
+            ) : (
+              <div className="w-0 dark:bg-secondary-dark-bg">
+                <Sidebar />
+              </div>
+            ))}
           <div
             className={
               activeMenu
